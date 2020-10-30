a5b26ec94b034688bc319712c7dccc8a4e14ccb9
mmichalski3 <mmichalski3@pl.sii.eu>
2020-10-30 01:44:09 +0100

    -drobne poprawki. -zwróć uwagę na konstrukcję String.format("Bearer %s", token) -trochę poczyściłem

--- src/main/java/contracts/AccountController.java
+++ src/main/java/contracts/AccountController.java
@@ -11,19 +11,27 @@ public class AccountController {
         Configuration configuration = new Configuration();
         return given().spec(configuration.getRequestSpecification()).contentType(ContentType.JSON).body(user).post("/Account/v1/User");
     }
-    public static Response generateToken(User user)
-    {
+
+    public static Response generateToken(User user) {
         Configuration configuration = new Configuration();
         return given().spec(configuration.getRequestSpecification()).contentType(ContentType.JSON).body(user).post("/Account/v1/GenerateToken");
     }
 
-    public static Response authorized(User user) {
+    public static Response authorized(User user, String token) {
         Configuration configuration = new Configuration();
-        return given().spec(configuration.getRequestSpecification()).contentType(ContentType.JSON).body(user).post("/Account/v1/Authorized");
+        return given()
+                .spec(configuration.getRequestSpecification())
+                .header("Authorization", String.format("Bearer %s", token))
+                .contentType(ContentType.JSON)
+                .body(user)
+                .post("/Account/v1/Authorized");
     }
 
-    public static Response deleteUser(String userID,String bearerToken) {
+    public static Response deleteUser(String userID, String bearerToken) {
         Configuration configuration = new Configuration();
-        return given().spec(configuration.getRequestSpecification()).header("Authorization","Bearer " + bearerToken).contentType(ContentType.JSON).post("/Account/v1/User/"+userID);
+        return given()
+                .spec(configuration.getRequestSpecification())
+                .header("Authorization", String.format("Bearer %s", bearerToken))
+                .contentType(ContentType.JSON).delete("/Account/v1/User/" + userID);
     }
 }
--- src/main/java/tests/UserTest.java
+++ src/main/java/tests/UserTest.java
@@ -1,71 +1,57 @@
 package tests;
 
 import contracts.AccountController;
-import contracts.Configuration;
 import io.restassured.response.Response;
-import objects.user.*;
+import objects.user.User;
+import objects.user.UserGenerateToken;
+import objects.user.UserResponse;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
-import org.hamcrest.Matchers;
 import org.junit.AfterClass;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-
 public class UserTest {
 
-    User user = new User("Damian16", "Qwerty123!");
-    static String userID;
-    static String token;
-    private void setUser() {
-        Response setUserResponse = AccountController.setUser(user);
-        UserResponse userBody = setUserResponse.then().extract().body().as(UserResponse.class);
-//        Response setUserResponse = AccountController.setUser(user);
-//        int statusCode = setUserResponse.statusCode();
-//        System.out.println(statusCode);
+    static User user = new User("Damian17", "Qwerty123!");
+    private static String userID;
+    private static String token;
+
+    private static UserResponse setUser() {
+        Response response = AccountController.setUser(user);
+        UserResponse userBody = response.then().extract().body().as(UserResponse.class);
+
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(userBody.getUserID()).isNotEqualTo("");
-        userID = userBody.getUserID();
         softAssertions.assertThat(userBody.getUsername()).isEqualTo(user.getUserName());
-        for (UserBooks item : userBody.getBooks()) {
-            softAssertions.assertThat(item.getIsbn()).isEmpty();
-            softAssertions.assertThat(item.getTitle()).isEmpty();
-            softAssertions.assertThat(item.getSubTitle()).isEmpty();
-            softAssertions.assertThat(item.getAuthor()).isEmpty();
-            softAssertions.assertThat(item.getPublish_date()).isEmpty();
-            softAssertions.assertThat(item.getPublisher()).isEmpty();
-            softAssertions.assertThat(item.getPages()).isEqualTo(0);
-            softAssertions.assertThat(item.getDescription()).isEmpty();
-            softAssertions.assertThat(item.getWebsite()).contains("http");
-        }
+        softAssertions.assertThat(userBody.getBooks().size()).isEqualTo(0);
         softAssertions.assertAll();
+        return userBody;
     }
 
-    private void generateToken() {
-        Response generateTokenUserResponse = AccountController.generateToken(user);
-        UserGenerateToken generateTokenInfo = generateTokenUserResponse.then().extract().body().as(UserGenerateToken.class);
-        token = generateTokenInfo.getToken();
+    private static UserGenerateToken generateToken() {
+        Response response = AccountController.generateToken(user);
+        UserGenerateToken generateTokenInfo = response.then().extract().body().as(UserGenerateToken.class);
+
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(generateTokenInfo.getStatus()).isEqualTo("Success");
+        softAssertions.assertThat(generateTokenInfo.getResult()).isEqualTo("User authorized successfully.");
         softAssertions.assertAll();
+
+        return generateTokenInfo;
     }
 
     @Test
-    public void authorizedUser()
-    {
-        setUser();
-        generateToken();
-        Response authorizedUserResponse = AccountController.authorized(user);
-        String authorization = authorizedUserResponse.then().extract().body().htmlPath().get().children().get(0).value();
-        SoftAssertions softAssertions = new SoftAssertions();
-        softAssertions.assertThat(authorization).isEqualTo("true");
-        softAssertions.assertAll();
+    public void authorizedUser() {
+        userID = setUser().getUserID();
+        token = generateToken().getToken();
+        Response response = AccountController.authorized(user, token);
+        String authorization = response.then().extract().body().as(String.class);
+
+        Assertions.assertThat(authorization).isEqualTo("true");
     }
 
     @AfterClass
     public static void deleteUser() {
-        Response deleteUserResponse = AccountController.deleteUser(userID,token);
-//        UserDelete deleteUserBody = deleteUserResponse.then().extract().body().as(UserDelete.class);
-        System.out.println(deleteUserResponse.getStatusCode());
-        System.out.println(deleteUserResponse.getBody());
+        AccountController.deleteUser(userID, token).then().statusCode(204);
     }
 }
